07507eafea83752521f8925dcd1c5b0bd9c9bab7
kobrakai415 <kai.kadir@hotmail.co.uk>
2021-09-28 16:41:54 +0100

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from "express"
+import SpotifyWebApi from "spotify-web-api-node"
+import cors from "cors"
+
+const server = express()
+server.use(cors())
+
+server.use(express.json())
+
+server.post("/login", (req, res) => {
+    console.log(req.body.code.code)
+    const code = req.body.code.code
+    
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: "http://localhost:3000",
+        clientId: "ede976d713b14614b050b8afe5c3ac0c",
+        clientSecret: "00e9ab0a66684138bcc0a27905299f29"
+    }
+    )
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in,
+        })
+        }).catch((error) => {
+            console.log(error)
+            res.sendStatus(400)
+    } )
+})
+
+server.listen(3001, () => { 
+    console.log(`Server listening on port: 3001`)
+})
